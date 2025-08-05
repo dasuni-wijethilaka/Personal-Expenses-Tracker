75e3a5852e8d00127fb56156b7c63636eafc94ff
dasuni_wijethilaka <dasunichethanadc@gmail.com>
2025-08-05 13:36:04 +0530

    add swagger UI

--- obj/Debug/net8.0/ExpenseTrackerAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExpenseTrackerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTrackerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86658b742b75eacd77f9a2ed8f614a705d29dde7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTrackerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTrackerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
